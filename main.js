de1ce89651043ec6619d7cf180e83067d87e25d0
Starcush <hacker0419@gmail.com>
2020-07-24 22:54:50 +0900

    add scroll event and make navigation bar

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 const prefix = document.querySelector('.prefix');
 const suffix = document.querySelector('.suffix');
-const point = document.querySelector('.point');
+// const point = document.querySelector('.point');
 const homeIntro = document.querySelector('.home-intro');
-console.log(homeIntro);
-const homeIntroComments = homeIntro.textContent;
 
 const makeReg = (str) => {
   return new RegExp(str, 'g');
+}
+
+let mouseoverFunc = (event) => { // 마우스를 가져다 대면 메인화면 글의 내용이 바뀌도록
+  // 정규표현식을 사용해서 원하는 className이 있는지 확인하는 걸 작성하자.
+  const cName = event.target.className;
+  // window.scrollTo(0, 1000);
+
+  const regObj = {
+    cho: makeReg('cho'),
+    jun: makeReg('jun'),
+    hyun: makeReg('hyun'),
+  };
+    
+  if (regObj.cho.test(cName)) {
+    suffix.textContent = '은?';
+    // point.style.color = '#E53A40';
+  } else if (regObj.jun.test(cName)) {
+    prefix.textContent = '지금까지'
+    suffix.textContent = '이 한 것들';
+    // point.style.color = '#EFDC05';
+  } else {
+    suffix.textContent = '과 연락은';
+    // point.style.color = '#30A9DE';
+  }
+
+  homeIntro.style.opacity = 0;
+}
+
+let mouseoutFunc = () => {
+  prefix.textContent = '';
+  suffix.textContent = '입니다.';
+  // point.style.color = '#FFFFFF';
+  homeIntro.style.opacity = 1;
 }
 
 const mouseEvents = {
@@ -22,36 +53,27 @@ names.forEach((el) => { // 여러 이벤트 추가
   }
 });
 
-let mouseoverFunc = (event) => { // 마우스를 가져다 대면 메인화면 글의 내용이 바뀌도록
-  // 정규표현식을 사용해서 원하는 className이 있는지 확인하는 걸 작성하자.
-  // console.log(event.target.className.match(/cho/g));
-  const cName = event.target.className;
-
-  const regObj = {
-    cho: makeReg('cho'),
-    jun: makeReg('jun'),
-    hyun: makeReg('hyun'),
-  };
-  
-  if (regObj.cho.test(cName)) {
-    suffix.textContent = '은?';
-    // point.style.color = '#E53A40';
-    homeIntro.textContent = '';
-  } else if (regObj.jun.test(cName)) {
-    prefix.textContent = '지금까지'
-    suffix.textContent = '이 한 것들';
-    // point.style.color = '#EFDC05';
-    homeIntro.textContent = '';
+let projectBackground = (progress) => {
+  let projectsSection = document.getElementById("projects"); 
+  if(progress > 56 && progress < 62) {
+    projectsSection.style.backgroundColor = '#000000';
   } else {
-    suffix.textContent = '과 연락';
-    // point.style.color = '#30A9DE';
-    homeIntro.textContent = '';
-  } 
+    projectsSection.style.backgroundColor = '#FFFFFF';
+  }
 }
 
-let mouseoutFunc = () => {
-  prefix.textContent = '';
-  suffix.textContent = '입니다.';
-  // point.style.color = '#FFFFFF';
-  homeIntro.textContent = homeIntroComments;
-}
+let last_known_scroll_position = 0;
+let ticking = false;
+
+window.addEventListener("scroll", function() {
+  last_known_scroll_position = window.scrollY;
+  progress = (window.pageYOffset / document.body.offsetHeight) * 100;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      projectBackground(progress);
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
