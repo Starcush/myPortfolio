d8fd94faaf3972ca238dc7099b78371938fa3637
Starcush <hacker0419@gmail.com>
2020-08-12 13:34:33 +0900

    ADD 프로젝트 영역에도 반응형 웹을 적용했습니다.
     - 아직 배경이 검정색으로 변경되는 지점을 정하는 것은 적용하지 못했습니다.

--- scrollArrow.js
+++ scrollArrow.js
@@ -16,11 +16,9 @@ const scrollArrow = (e) => {
   let curPosition = window.scrollY;
 
   if(findClassName(cName, 'main-scroll-arrow')) {
-    console.log('main page arrow');
     window.scrollTo({top: introLocation, behavior:'smooth'});  
 
   } else if(findClassName(cName, 'scroll-up')) {
-    console.log('page up');
 
     if(curPosition <= introLocation) {
       window.scrollTo({top: 0, behavior: 'smooth'});
@@ -31,7 +29,6 @@ const scrollArrow = (e) => {
     }
 
   } else if(findClassName(cName, 'scroll-down')) {
-    console.log('page down');
 
     if(curPosition <= introLocation) {
       window.scrollTo({top: projectLocation, behavior: 'smooth'});
@@ -55,9 +52,8 @@ let ticking = false;
 
 window.addEventListener("scroll", function() {
   let progress = (window.pageYOffset / document.body.offsetHeight) * 100;
-  console.log(progress);
+  
   if(progress < 7) {
-    console.log('fade out');
     arrowDiv.classList.add('fade-out');
     arrowDiv.classList.remove('fade-in');
   } else {
